a127ea4d64a7d9c1894dd66773cc7250487affde
Pablo Corbalán <corbalandeconpablo@gmail.com>
2021-01-20 18:24:24 +0100

    feat(makefile): makefile created for making JUST the trojan horse
    USE: cd .client/trojan; make

--- client/trojan/trojan.c
+++ client/trojan/trojan.c
@@ -6,43 +6,50 @@
 #include <netinet/in.h> // Adress family (AF_INET)A
 
 #define IP_ADDRESS "127.0.1.1"
-#define PORT 4444
+#define PORT 8080
+
 /*
+ * Actually starts the socket connection of the client using the tcp
+ * model and server sockets.
+ */
 int socketStart()
 {
   struct sockaddr_in serv;
   int fd, conn; // FD stands for file descriptor (the descriptor of the socket)
-  char message [100]; // Stores messages sents from the server 
+  char message [100] = ""; // Stores messages sents from the server 
   // Set up the actual socket and then bind it to the server
   fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd != 0)
+  if (fd < 0)
   {
     printf("Can't create the socket...\n");
   }
   else 
     printf("Socket created successfuly...\n");
   serv.sin_family = AF_INET;
-  serv.sin_port = htons(4444);
-  if ((inet_pton(AF_INET, "127.0.0.1", &serv.sin_addr)) != 0)
-  {
-    printf("Client can't be binded to the server\n");
-  }
-  else
-    printf("Client binded to the server...\n");
+  serv.sin_port = htons(PORT);
+  inet_pton(AF_INET, IP_ADDRESS, &serv.sin_addr); // this line binds the client
   // Start the chat while loop
   while (1)
   {
     printf("Enter a message to the server: ");
     fgets(message, 100, stdin);
-    send(fd, message, strlen(message), 0);
+    send(fd, message, strlen(message), 0); 
   }
 }
-*/
+
+/*
+ * Runs a small socket test I developed for trying if all was ok in 
+ * the tcp connection and the tcp server.
+ *
+ * From the server you can run:
+ *  - python3 server/stest.py
+ */
 int socketTest() 
 {
   struct sockaddr_in serv;
   int fd;
-  int conn; char message[100] = ""; 
+  int conn;
+  char message[100] = ""; 
   fd = socket(AF_INET, SOCK_STREAM, 0);
   serv.sin_family = AF_INET;
   serv.sin_port = htons(PORT);
@@ -59,7 +66,6 @@ int socketTest()
 
 int main()
 {
-  // socketStart();
   socketTest();
   return 0;
 }
