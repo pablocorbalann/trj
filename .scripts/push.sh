e4ffd91036c2c38913a80cb5966a2622d2195ca3
Pablo Corbalán <corbalandeconpablo@gmail.com>
2021-01-27 09:28:31 +0100

    feat: Basic

---
+++ .scripts/push.sh
@@ -0,0 +1,16 @@
+# This script is used to push all the changes to the github repository.
+# Before pushing the changes it removes all the cache options as well 
+# as the C object options.
+#
+# To run this script use:
+#   sh push.sh <commit-flag> <commit-msg>
+if ![ -x "$(command -v git)"]
+then
+  echo "ERROR: git is not installed..."
+  exit 1
+fi 
+git pull
+rm -rf client/trojan/*.o
+git add .
+git commit -m "$1: $2"
+git push
