fef0e715735efd9380b2ce4c5bce8bc8673fb51c
Pablo Corbalán <corbalandeconpablo@gmail.com>
2021-02-10 15:47:10 +0100

    test: first open CV test implementations

---
+++ client/ntrj.test.c
@@ -0,0 +1,12 @@
+/*
+ * This small test has been created for trying to open a picture in C. 
+ *
+ * As this is a learning proyect, sometimes I have to create this small 
+ * files that contain examples of basic things.
+ *
+ * For more information: github.com/pblcc/trj
+ *
+ * Follow me on Twitter: @pablocorbcon
+ */
+#include <stdio.h>
+#include <cv.h>
