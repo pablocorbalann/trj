73638ee0765b4b8e33231e491eba7b447fe4b65e
Mateusz Zaborski <mateusz.zaborski@assaabloy.com>
2021-02-14 17:46:30 -0500

    Cleaned makefiles

--- client/test/test.mk
+++ client/test/test.mk
@@ -1,8 +1,6 @@
-# Find all the C files we want to compile
 TEST_SRCS += $(shell find . -path "./test/*.c")
-
 TEST_OBJS := $(TEST_SRCS:%.c=$(BUILD_DIR)/%.o)
 
 PKGCONFIG = $(shell which pkg-config)
-CFLAGS = $(shell $(PKGCONFIG) --cflags gtk+-2.0)
-TEST_LDFLAGS = $(shell $(PKGCONFIG) --libs gtk+-2.0)
+CFLAGS += $(shell $(PKGCONFIG) --cflags gtk+-2.0)
+TEST_LIBS := $(shell $(PKGCONFIG) --libs gtk+-2.0)
--- client/trojan/trojan.mk
+++ client/trojan/trojan.mk
@@ -1,4 +1,2 @@
-# Find all the C files we want to compile
 SRCS += $(shell find . -path "./trojan/*.c")
-
 OBJS := $(SRCS:%.c=$(BUILD_DIR)/%.o)
